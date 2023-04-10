5e68fd34e1d95e894d271152f6db2cae052de6ee
Angel Brand <AngelBrand1>
2023-04-10 13:03:24 -0500

    abr 10

--- Player.py
+++ Player.py
@@ -5,14 +5,12 @@
 #---------------------------------------
 
 class Player:
-    players = []
 
-    def __init__(self, fichas, nombre):
-        self._fichas = fichas #Array con fichas
-        self._nombre = nombre #Los virtuales tienen nombres predefinidos
+    def __init__(self,nombre = None):
+        self._fichas = [] #Array con fichas
+        self._nombre = nombre if nombre else "" #Los virtuales tienen nombres predefinidos
         self._turno = False #Cambia a True si es su turno
         self._numero_fichas = 0 #Se debe mostrar en pantalla
-        self.players.append(self) #Agregar cada jugador a un array
 
     def getFichas(self):
         return self._fichas
---
+++ main.py
@@ -0,0 +1,63 @@
+from Ficha import *
+from Player import *
+from random import sample, randrange
+
+
+
+
+def llenarFichas():
+    """llena una a una las 28 fichas"""
+    fichas:Ficha = []
+    for i in range(7):
+        for j in range(i,7):
+            fichas.append(Ficha(i,j))
+    return fichas
+def repartirFichas(fichas,arr=[]):
+    """reparte 7 fichas para cada jugador que viene en el array arr"""
+    players = []
+    for player in arr:
+        fichas_selec = sample(fichas, 7)
+        fichas = list(filter(lambda x: x not in fichas_selec,fichas))
+        player.setFichas(fichas_selec)
+        players.append(player)
+    return players
+
+def verificarSeis(arr):
+    for i in range(len(arr)):
+        j=0
+        for ficha in arr[i].getFichas():
+            if ficha.getA()+ficha.getB() == 12:
+                return i,arr[i].getFichas().pop(j)
+            j+=1
+def jugada(player,turno,fichasJugadas):
+    cont =1
+    fichas = player.getFichas()
+    for ficha in fichas:
+        print(f"ficha #{cont}: {ficha.getA()}:{ficha.getB()}")
+        cont+=1
+    
+    numFicha = int(input("elija una ficha a jugar: ")) if turno==3 else randrange(len(fichas))
+    fichasJugadas.append(fichas.pop(numFicha-1))
+"""main: se lleva a cabo la partida"""
+if __name__ == "__main__":
+    fichasJugadas =[]
+    fichas = llenarFichas()
+    players = [Player(),Player(),Player(),Player(input("ingrese su nombre: "))]
+    players = repartirFichas(fichas, players)
+    ver = verificarSeis(players)
+    turno=ver[0]
+    fichasJugadas.append(ver[1])
+    print(fichasJugadas)
+    print(f"inicia el jugador: virtual {turno+1}" if turno!=3 
+          else f"inicia el jugador:{players[turno].getNombre()}" )
+    while True:
+        turno=turno+1 if turno!=3 else 0
+        print(f"turno para jugador: virtual {turno+1}" if turno!=3 
+              else f"turno para jugador: {players[turno].getNombre()}")
+        jugada(players[turno],turno,fichasJugadas)
+        
+
+
+
+    
+    
