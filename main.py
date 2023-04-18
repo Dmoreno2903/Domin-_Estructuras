9ee4e1308f35523e34821d99ad5f39342c8e4fb5
SMOLINAV <111089184+SMOLINAV@users.noreply.github.com>
2023-04-18 00:55:08 -0500

    tapicuplayer

--- main.py
+++ main.py
@@ -29,7 +29,32 @@ def verificarSeis(arr,partida):
                 partida.fichaDerecha(fichas.pop(j))
                 return i
             j+=1
-            
+
+def tapicu(fichas, partida,indice_1, indice_2):
+    for ficha in fichas:
+        fichas_dobles = list(filter(lambda x: x.getA() == x.getB(), fichas))
+        A, B = partida.getPosibles()
+        fichas_posibles = list(filter(lambda x: x.getA() == A or x.getA() == B, fichas_dobles))
+    if len(fichas_posibles) >= 2:
+        for i in range(0, len(fichas_posibles)):
+            if fichas[indice_1]== fichas_posibles[i]:
+                for j in range(0, len(fichas_posibles)):
+                    if fichas[indice_2]== fichas_posibles[j]:
+                        A, B = partida.getPosibles()
+                        fichas_dobles = list(filter(lambda x: x.getA() == x.getB(), fichas))
+                        fichasIz = list(filter(lambda x: x.getA()  == A, fichas_dobles))
+                        fichasDe = list(filter(lambda x: x.getA() == B, fichas_dobles))
+                        if len(fichasIz) == 1 and len(fichasDe) == 1:
+                            fichaIz = fichasIz[0]
+                            fichasDer = fichasDe[0]
+                            partida.fichaIzquierda(fichaIz), fichas.remove(fichaIz)
+                            partida.fichaDerecha(fichasDer), fichas.remove(fichasDer)
+                            print("-- Tapicú --")
+    else:
+        print("-"*100)
+        print("Jugada inválida, pierdes turno")
+        print("-"*100)
+
 def validarJugada(fichas,indice,lado,partida):
     ficha = fichas[indice]
     A, B = partida.getPosibles()
@@ -69,16 +94,40 @@ def jugada(player,turno,partida):
         for ficha in fichas:
             print(f"{cont}. [{ficha.getA()}|{ficha.getB()}]")
             cont+=1
+        print(f"{cont}. Tapicu")    
         print("\n", "-"*100, sep="")
         indice= int(input("\n-> Selecione una ficha a jugar: "))-1
         print("\n", "-"*100, sep="")
         if indice<len(fichas) and indice>=0:
             print("-- Seleccione D para poner la ficha en el lado derecho del tablero --")
-            print("-- seleccione I para poner la ficha en el lado izquierdo del tablero --")
+            print("-- Seleccione I para poner la ficha en el lado izquierdo del tablero --")
             print("-"*100, "\n", sep="")
             lado= input("-> Seleccione lado: ")
             print("\n", "-"*100, sep="")
-            validarJugada(fichas,indice,lado,partida)
+            validarJugada(fichas, indice, lado, partida)
+        elif indice == len(fichas):
+            print("\n", "-"*100, sep="")
+            indice_1= int(input("\n-> Seleciona la ficha 1: "))-1
+            print("\n", "-"*100, sep="")
+            if indice_1<len(fichas) and indice_1>=0:
+                print("-- Seleccione D para poner la ficha en el lado derecho del tablero --")
+                print("-- Seleccione I para poner la ficha en el lado izquierdo del tablero --")
+                print("-"*100, "\n", sep="")
+                lado= input("-> Seleccione lado: ")
+                print("\n", "-"*100, sep="")
+            print("\n", "-"*100, sep="")
+            indice_2= int(input("\n-> Seleciona la ficha 2: "))-1
+            print("\n", "-"*100, sep="")
+            if indice_2<len(fichas) and indice_2>=0:
+                print("-- Seleccione D para poner la ficha en el lado derecho del tablero --")
+                print("-- Seleccione I para poner la ficha en el lado izquierdo del tablero --")
+                print("-"*100, "\n", sep="")
+                lado= input("-> Seleccione lado: ")
+                print("\n", "-"*100, sep="")
+
+                tapicu(fichas, partida,indice_1, indice_2)
+                
+    
         else:
             if indice == -1:
                 print("¡Pasaste de turno!")
